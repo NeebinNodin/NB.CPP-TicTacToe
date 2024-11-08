5f8ee20b0360938750329b720d78342fefa378c2
NeebinNodin <164468785+NeebinNodin@users.noreply.github.com>
2024-11-07 22:22:06 -0600

    It works and got most of the bugs out. Did include input validation for char/strings but commented it out since it's not covered in class and I don't fully understand it..

--- CPP-TicTacToe/TicTacToe.cpp
+++ CPP-TicTacToe/TicTacToe.cpp
@@ -1,6 +1,7 @@
 
 #include "TicTacToe.h"
 #include <iostream>
+//#include <limits> not covered in class
 
 bool TicTacToe::IsGameOver() const
 {
@@ -8,25 +9,27 @@ bool TicTacToe::IsGameOver() const
 	// 012
 	// 345
 	// 678
-	if((m_board[0] == m_board[1] && m_board[1] == m_board[2])
-		|| (m_board[3] == m_board[4] && m_board[4] == m_board[5])
-		|| (m_board[6] == m_board[7] && m_board[7] == m_board[8])
-		|| (m_board[0] == m_board[3] && m_board[3] == m_board[6])
-		|| (m_board[1] == m_board[4] && m_board[4] == m_board[7])
-		|| (m_board[2] == m_board[5] && m_board[5] == m_board[8])
-		|| (m_board[0] == m_board[4] && m_board[4] == m_board[8])
-		|| (m_board[2] == m_board[4] && m_board[4] == m_board[6]))
+	if((m_board[0] == m_board[1] && m_board[1] == m_board[2] && m_board[0] != ' ')
+		|| (m_board[3] == m_board[4] && m_board[4] == m_board[5] && m_board[3] != ' ')
+		|| (m_board[6] == m_board[7] && m_board[7] == m_board[8] && m_board[6] != ' ')
+		|| (m_board[0] == m_board[3] && m_board[3] == m_board[6] && m_board[0] != ' ')
+		|| (m_board[1] == m_board[4] && m_board[4] == m_board[7] && m_board[1] != ' ')
+		|| (m_board[2] == m_board[5] && m_board[5] == m_board[8] && m_board[2] != ' ')
+		|| (m_board[0] == m_board[4] && m_board[4] == m_board[8] && m_board[0] != ' ')
+		|| (m_board[2] == m_board[4] && m_board[4] == m_board[6] && m_board[2] != ' '))
 	{
-		std::cout << "Player " << m_player << " wint!\n";
+		// determine who won
+		if(m_player == 1) std::cout << "Player 2 wins!\n";
+		else std::cout << "Player 1 wins!\n";
 		return true;
 	}
 	else if(m_turn == 9)
 	{
+		// if 9 turns is reached, game ends in a tie since there are no more moves available
 		std::cout << "The game has ended in a tie!\n";
+		return true;
 	}
 	else return false;
-		
-	
 }
 
 void TicTacToe::TakeTurn()
@@ -35,20 +38,30 @@ void TicTacToe::TakeTurn()
 	bool validMove = false;
 	while(!validMove)
 	{
+		// Ask player to enter a move
 		std::cout << "Player " << m_player << " enter a move: ";
 		std::cin >> move;
-		if (move < 1 || move > 9)
-		{
-			std::cout << "Invalid move, try again.\n";
+
+		/*input validation if a char/string is entered
+		not covered in class so commented out. Does work though.
+		
+		if(std::cin.fail()){
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Invalid input, please enter a number between 1 and 9.\n";
+			continue;
 		}
+		*/
+
+		if (move < 1 || move > 9){std::cout << "Invalid move, try again.\n";}
 		else if(m_board[move - 1] == 'X' || m_board[move - 1] == 'O')
 		{
 			std::cout << "That space is already taken, try again.\n";
 		}
 		else
 		{
-			m_board[move - 1] = (m_player == 1) ? 'X' : 'O';
-			m_player = (m_player == 1) ? 2 : 1;
+			m_board[move - 1] = (m_player == 1) ? 'X' : 'O'; // Place player's symbol on board
+			m_player = (m_player == 1) ? 2 : 1; // switch players for each turn
 			m_turn++;
 			validMove = true;
 		}
@@ -57,7 +70,7 @@ void TicTacToe::TakeTurn()
 
 void TicTacToe::Display() const
 {
-	system("cls");
+	system("cls"); // clears the display and displays the move player just made.
 	std::cout << m_board[0] << " | " << m_board[1] << " | " << m_board[2] << "\n";
 	std::cout << "---------\n";
 	std::cout << m_board[3] << " | " << m_board[4] << " | " << m_board[5] << "\n";
